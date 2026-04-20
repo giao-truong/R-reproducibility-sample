80d1d1d46cb8d04b5edb84b8fec0219fd21e6b7f
Giao Truong <truongquynhgiao1401@gmail.com>
2026-04-20 22:28:24 +0300

    Create master.R

---
+++ master.R
@@ -0,0 +1,11 @@
+# Master script
+
+rm(list = ls())
+
+source("code/01_setup.R")
+source("code/02_clean.R")
+source("code/03_construct_indicators.R")
+source("code/04_analysis.R")
+source("code/05_visualization.R")
+
+cat("Pipeline completed successfully.\n")
