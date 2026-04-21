3a7300c03c5bc765fe72d560e33e3a76b116b425
Giao Truong <truongquynhgiao1401@gmail.com>
2026-04-21 20:50:36 +0300

    Update master.R

--- master.R
+++ master.R
@@ -8,7 +8,7 @@ rm(list = ls())
 
 source("code/01_setup.R")
 source("code/02_clean.R")
-source("code/03_construct_indicators.R")
+source("code/03_feature_engineering.R")
 source("code/04_analysis.R")
 source("code/05_visualization.R")
 
