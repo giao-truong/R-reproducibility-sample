6b6affc06532110a3f5cac31006d54ef31ddf991
Giao Truong <truongquynhgiao1401@gmail.com>
2026-04-21 08:51:30 +0300

    Update 02_clean.R

--- code/02_clean.R
+++ code/02_clean.R
@@ -1,22 +1,27 @@
-# =========================================
+### ------------------- ###
+### 02. Data Cleaning  ###
+### ------------------- ###
 
-# Data Cleaning
+source("code/01_setup.R")
 
-# =========================================
+# Load data
+data <- read_excel(data_path)
 
-data <- read_excel(here("data", "raw", "data.xlsx"))
-
+# Clean & reshape
 df <- data %>%
-rename(country = 1) %>%
-pivot_longer(
-cols = -country,
-names_to = "date",
-values_to = "credit_gdp"
-) %>%
-mutate(
-date = ymd(date),
-year = year(date)
-) %>%
-arrange(country, date)
+  rename(country = 1) %>%
+  pivot_longer(
+    cols = -country,
+    names_to = "date",
+    values_to = "credit_gdp"
+  ) %>%
+  mutate(
+    date = ymd(date),
+    year = year(date),
+    qtr  = quarter(date),
+    year_q = sprintf("%dQ%d", year, qtr)
+  ) %>%
+  arrange(country, date)
 
-write_csv(df, here("data", "cleaned", "panel.csv"))
+# Save cleaned data
+saveRDS(df, file.path(root, "data/clean_df.rds"))
